2e5116f7d3ece3a42b8804444e8f1a79e2a9aa6a
MuhammadFauzandotexe <mfauzan51087@gmail.com>
2023-07-22 16:23:59 +0700

    initialize

---
+++ src/main/java/org/ojan/tokobagus/TokobagusApplication.java
@@ -0,0 +1,13 @@
+package org.ojan.tokobagus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokobagusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokobagusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ojan/tokobagus/TokobagusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ojan.tokobagus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokobagusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
